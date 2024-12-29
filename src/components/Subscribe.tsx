7e7ac402734ad08cff810e7231a67a0a6cd2931f
Akash Singh <developer.akash8@gmail.com>
2024-12-29 20:18:41 +0530

    component: added for email subscription

---
+++ src/components/Subscribe.tsx
@@ -0,0 +1,35 @@
+"use client";
+
+import { useState } from "react";
+import styles from "@/styles/subscribe.module.scss";
+
+const Subscribe = () => {
+  const [email, setEmail] = useState("");
+
+  const handleSubscribe = (e: React.SyntheticEvent) => {
+    e.preventDefault();
+
+    console.log("Email :", email);
+
+    setEmail("");
+  };
+
+  return (
+    <div className={styles.subscribe}>
+      <h2>Stay ahead with latest Winlock updates</h2>
+
+      <form onSubmit={handleSubscribe}>
+        <input
+          type="email"
+          placeholder="Enter your email"
+          required
+          value={email}
+          onChange={(e) => setEmail(e.target.value)}
+        />
+        <button type="submit">Subscribe now</button>
+      </form>
+    </div>
+  );
+};
+
+export default Subscribe;
