4d351bad8ff6a790811419f88c6bab53cd1fe8b1
Akash Singh <developer.akash8@gmail.com>
2024-12-29 16:10:04 +0530

    component: added Footer

---
+++ src/components/Footer.tsx
@@ -0,0 +1,29 @@
+import Image from "next/image";
+
+import styles from "@/styles/footer.module.scss";
+
+import github from "@/assets/socials/github.svg";
+import twitter from "@/assets/socials/twitter-x.svg";
+import linkedin from "@/assets/socials/linkedin.svg";
+
+const Footer = () => {
+  return (
+    <div className={styles.footer}>
+      <p>&copy; 2025 Winlock. All rights reserved</p>
+
+      <div className={styles.socials}>
+        <a href="https://github.com/akash-singh8/winlock-web" target="_blank">
+          <Image src={github} alt="Github" />
+        </a>
+        <a href="https://x.com/dev_skyi" target="_blank">
+          <Image src={twitter} alt="Twitter" />
+        </a>
+        <a href="https://www.linkedin.com/in/akash-singh8/" target="_blank">
+          <Image src={linkedin} alt="Linkedin" />
+        </a>
+      </div>
+    </div>
+  );
+};
+
+export default Footer;
